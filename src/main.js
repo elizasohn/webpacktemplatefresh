afdf0e4243c0be852a05d690f7d63cb8c34e1ab8
Eliza Sohn <eliza.sohn@gmail.com>
2019-11-17 23:05:55 -0800

    create webpack template

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { pingPong } from './project';
+import 'bootstrap';
+import './styles.css';
+
+$(document).ready(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+    var goal = $('#goal').val();
+    var output = pingPong(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
